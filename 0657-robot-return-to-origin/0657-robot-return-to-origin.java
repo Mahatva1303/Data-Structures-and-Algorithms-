9e09f6ed24a26d84dd8be0992c79d0539c84ef38
Mahatva Agarwal <mahatvaagarwal15@gmail.com>
2026-07-14 10:46:48 +0530

    Time: 5 ms (68.78%), Space: 46.1 MB (38.52%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for(char m : moves.toCharArray()){ // converts string to character array
+            if(m == 'R'){
+                x++;
+            }
+            else if(m == 'L'){
+                x--;
+            }
+            else if(m == 'U'){
+                y++;
+            }
+            else{
+                y--;
+            }
+
+        }
+        return x == 0 && y==0;
+    }
+}
