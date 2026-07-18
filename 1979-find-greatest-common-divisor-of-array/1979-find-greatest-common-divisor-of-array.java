e8830bd02d4f19f5eb5a14aeed814d4dbf289ce3
Mahatva Agarwal <mahatvaagarwal15@gmail.com>
2026-07-18 15:32:15 +0530

    Time: 0 ms (100%), Space: 44.8 MB (94.14%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -3,16 +3,16 @@ class Solution {
         int smallest = nums[0];
         int greatest = nums[0];
         for(int i = 1;i < nums.length;i++){
-            if(nums[i] < smallest){
+            if(nums[i] < smallest){ // smallest number calculation in array
                 smallest = nums[i];
             }
 
-            if(nums[i] > greatest){
+            if(nums[i] > greatest){ //greatest number calculaion in array
                 greatest = nums[i];
             }
         }
-
-            while(greatest != 0){
+// if you take while loop inside for loop it will not aloww to check full array 
+            while(greatest != 0){ // GCD calculation
                 int temp = greatest;
                 greatest = smallest % greatest;
                 smallest = temp;
